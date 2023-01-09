b7f3154c52d535fda56e542172f50d8614fb9cb9
Philip Huang <philiphuang2@gmail.com>
2023-01-09 11:59:38 -0800

    base dictionary lookup solved

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+import pkg from 'word-graphs';
+import fs from 'fs';
+
+const {MinimalWordGraph, QueryBuilder} = pkg;
+
+
+let mwg = new MinimalWordGraph();
+try {
+    const data = fs.readFileSync('./dictionary/dictionary.txt', 'utf8');
+    data.split(/\r?\n/).forEach(line=> {
+        mwg.add(line)
+    })
+    mwg.makeImmutable();
+    let test = ['c','a','t'];
+    console.log(mwg.lookup('cta'))
+    // console.log(mwg.containsAll(["c","a","t"]));
+} catch(err){
+    console.error(err);
+}
